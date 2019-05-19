a06eaac0b25de97b8bd87ee2368dcbf73694a57f
RISHAB <rkmr039@gmail.com>
2019-05-19 14:16:53 +0530

    Navigation to menu items done"
    This reverts commit 955698e0

--- app/src/main/java/com/labs/dotmenu/MainActivity.java
+++ app/src/main/java/com/labs/dotmenu/MainActivity.java
@@ -1,6 +1,5 @@
 package com.labs.dotmenu;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -19,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu,menu);
-        return super.onCreateOptionsMenu(menu);
+        return true;
     }
     // assign action to menu items
     @Override
@@ -27,20 +26,23 @@ public class MainActivity extends AppCompatActivity {
 
         int id = item.getItemId();
 
-        if(id == R.id.setings) {
-            Intent settingIntent = new Intent(MainActivity.this, SettingsActivity.class);
-            startActivity(settingIntent);
-            return false;
+        switch(id)
+        {
+            case R.id.setings:
+                Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
+                break;
+
+
+            case R.id.share:
+                 Toast.makeText(this,"Share", Toast.LENGTH_SHORT).show();
+                 break;
+
+            case R.id.feedback:
+                Toast.makeText(this,"Feedback", Toast.LENGTH_SHORT).show();
+                break;
+
         }
-        else if(id == R.id.share) {
-            Toast.makeText(this,"Share", Toast.LENGTH_SHORT).show();
-            return false;
-        }
-        else{
-            //case R.id.feedback:
-            Intent feedbackIntent = new Intent(MainActivity.this, FeedbackActivity.class);
-            startActivity(feedbackIntent);
-            return false;
-        }
+
+        return true;
     }
 }

f234e55d23ce3ff2b4e9218868df7af08b9e8ffd
RISHAB <rkmr039@gmail.com>
2019-05-19 14:18:32 +0530

    Navigation to menu items done

--- app/src/main/java/com/labs/dotmenu/MainActivity.java
+++ app/src/main/java/com/labs/dotmenu/MainActivity.java
@@ -1,5 +1,6 @@
 package com.labs.dotmenu;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -18,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu,menu);
-        return true;
+        return super.onCreateOptionsMenu(menu);
     }
     // assign action to menu items
     @Override
@@ -26,23 +27,20 @@ public class MainActivity extends AppCompatActivity {
 
         int id = item.getItemId();
 
-        switch(id)
-        {
-            case R.id.setings:
-                Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
-                break;
-
-
-            case R.id.share:
-                 Toast.makeText(this,"Share", Toast.LENGTH_SHORT).show();
-                 break;
-
-            case R.id.feedback:
-                Toast.makeText(this,"Feedback", Toast.LENGTH_SHORT).show();
-                break;
-
+        if(id == R.id.setings) {
+            Intent settingIntent = new Intent(MainActivity.this, SettingsActivity.class);
+            startActivity(settingIntent);
+            return false;
         }
-
-        return true;
+        else if(id == R.id.share) {
+            Toast.makeText(this,"Share", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        else{
+            //case R.id.feedback:
+            Intent feedbackIntent = new Intent(MainActivity.this, FeedbackActivity.class);
+            startActivity(feedbackIntent);
+            return false;
+        }
     }
 }
